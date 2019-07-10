b8200eb1c64b9e6f308693231923997047711aa1
Tamer M <tamermurtazaoglu@gmail.com>
2019-07-11 01:26:09 +0300

    Added attributes to devise podcast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+	protect_from_forgery with: :exception
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email,:password,:title, :password_confirmation)}
+		devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email,:password,:password_confirmation,:current_password,:title,:description,:itunes,:stitcher, :podbay)}
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_185336) do
+ActiveRecord::Schema.define(version: 2019_07_10_213627) do
 
   create_table "podcasts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 2019_07_10_185336) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
+    t.string "itunes"
+    t.string "stitcher"
+    t.string "podbay"
     t.index ["email"], name: "index_podcasts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_podcasts_on_reset_password_token", unique: true
   end
