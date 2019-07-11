280693e6ea1b511c98da7721a21e46ea3971fcde
Tamer M <tamermurtazaoglu@gmail.com>
2019-07-11 05:29:27 +0300

    Add podcast controller & views

---
+++ app/controllers/podcasts_controller.rb
@@ -0,0 +1,10 @@
+class PodcastsController < ApplicationController
+	
+	def index
+		@podcasts = Podcast.all.order("created_at DESC")
+	end
+
+	def show
+		@podcast = Podcast.find(params[:id])
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :podcasts
+	devise_for :podcasts
 	root 'welcome#index'
+
+	resources :podcasts, only: [:index,:show]
+
 end
